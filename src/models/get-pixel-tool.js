82c2f8a4fa466401823068ca0f0edcd851d47aa4
Dao Quang Phuong <daoquangphuong@gmail.com>
2022-09-08 00:37:01 +0700

    update

--- src/models/get-pixel-tool.js
+++ src/models/get-pixel-tool.js
@@ -3,10 +3,14 @@ const adb = require('./adb');
 const main = async () => {
   const buf = await adb.screenCapture();
   const input = [
-    [2314, 52], // #0a8ced
-    [2343, 52], // #0a8ced
-    [2313, 85], // #0a8ced
-    [2341, 83], // #0a8ced
+    [864, 766], // #ed4722
+    [1193, 769], // #eb441e
+    [854, 847], // #ea431d
+    [1179, 850], // #e9411b
+    [1315, 764], // #72ce19
+    [1639, 769], // #67c70e
+    [1314, 849], // #64c50b
+    [1633, 848], // #5ec205
   ];
   const { points } = await adb.getPixelColor(buf, input);
   console.info(input, ',');
